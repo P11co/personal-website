d8ca29e8e701377cb5a4459f1d0556d66ffec5ce
p11co <39766228+P11co@users.noreply.github.com>
2025-12-04 20:00:36 -0800

    fix: basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/personal-website',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-  // Uncomment and set this if your repo name is not username.github.io
-  // basePath: '/your-repo-name',
 }
 
 export default nextConfig
